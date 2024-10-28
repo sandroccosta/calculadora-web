c8e446e53a1d5695e16bbbc6078933740d8926d1
Alexsandro costa <89763074+sandroccosta@users.noreply.github.com>
2024-10-28 10:26:31 -0300

    Configuração da aplicação

---
+++ src/main/java/com/sandrodev/calculadora_web/CalculadoraWebApplication.java
@@ -0,0 +1,13 @@
+package com.sandrodev.calculadora_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandrodev/calculadora_web/CalculadoraWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandrodev.calculadora_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
